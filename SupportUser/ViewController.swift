cd579d63c5c4b358d8301cfe65c65cbb676eb77c
Vitaliy Kozlov <sfenke@gmail.com>
2020-02-05 08:39:16 +0300

    Init

---
+++ SupportUser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SupportUser
+//
+//  Created by Виталий Витальевич on 05.02.2020.
+//  Copyright © 2020 Vitaliy Kozlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
